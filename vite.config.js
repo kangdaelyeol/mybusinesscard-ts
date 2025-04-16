ad991470892a538fa979960c51a1de63f1d9ca46
rkdeofuf <asa47972000@naver.com>
2025-04-16 19:57:20 +0900

    Chore: add rollup-plugin-visualizer
    bundle 크기를 측정하기 위한 패키지 추가

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths(), tailwindcss()],
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        tailwindcss(),
+        visualizer({
+            filename: 'dist/stats.html',
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+        }),
+    ],
 })
