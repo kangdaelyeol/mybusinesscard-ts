2de4285fe7806036e3bfe844b55d46ad622bd300
rkdeofuf <asa47972000@naver.com>
2025-04-17 18:05:18 +0900

    Fix: CardEditorFallback component UI
    fallback 컴포넌트 사이즈 조정

--- src/components/fallback/card-editor-fallback.tsx
+++ src/components/fallback/card-editor-fallback.tsx
@@ -1,7 +1,7 @@
 export const CardEditorFallback = () => {
     return (
-        <div className="flex justify-center items-center h-[230px]">
-            <div className="h-full w-full flex justify-center items-center gap-[5px]">
+        <div className="flex justify-center items-center w-full h-[196px] my-[17px]">
+            <div className="h-full w-[440px] flex justify-center items-center gap-[5px]">
                 <svg
                     className="animate-spin shrink-0 h-[50px] w-[50px] mr-1 border-white border-[3px] border-t-transparent rounded-[50%]"
                     viewBox="0 0 24 24"
